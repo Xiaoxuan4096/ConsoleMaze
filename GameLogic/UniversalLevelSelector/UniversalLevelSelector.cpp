8418533dd3f17e908fc61337f239bfacc9416a44
Xiaoxuan4096 <232077189+Xiaoxuan4096@users.noreply.github.com>
2026-08-31 22:04:06 +0800

    添加迷宫生成逻辑与统一选关逻辑的占位符。准备构建发布版v1.2.3(Build 1.2.9739.25157)。

---
+++ GameLogic/UniversalLevelSelector/UniversalLevelSelector.cpp
@@ -0,0 +1,11 @@
+// UniversalLevelSelector.cpp - A unified level selector for gaming and editing.
+// Written by Xiaoxuan4096.
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#include "UniversalLevelSelector.h"
+
+namespace Xiaoxuan4096 {
+	// TODO: Migrate level selecting logic to here.
+}
---
+++ GameLogic/UniversalLevelSelector/UniversalLevelSelector.h
@@ -0,0 +1,11 @@
+// UniversalLevelSelector.h - A unified level selector for gaming and editing.
+// Written by Xiaoxuan4096.
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace Xiaoxuan4096 {
+	// TODO: Migrate level selecting logic to here.
+}
